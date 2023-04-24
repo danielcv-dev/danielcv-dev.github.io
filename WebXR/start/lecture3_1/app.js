80c4cfe212ab6c0aedb6f69ab195084be852c0cd
daniel vilchis <daniel.vilchis@accenture.com>
2023-04-24 12:34:32 -0600

    no message

--- WebXR/start/lecture3_1/app.js
+++ WebXR/start/lecture3_1/app.js
@@ -53,7 +53,9 @@ class App{
     
     initScene(){
         this.radius = 0.08;
-		this.room = new THREE.LineSegments(new BoxLineGeometry(6,6,610,10,10), new THREE.LineBasicMaterial({color: 0x808080}));
+		this.room = new THREE.LineSegments(
+					new BoxLineGeometry(6,6,6,10,10,10), 
+					new THREE.LineBasicMaterial({color: 0x808080}));
 		this.room.geometry.translate(0,3,0);
 		this.scene.add(this.room);
 		
