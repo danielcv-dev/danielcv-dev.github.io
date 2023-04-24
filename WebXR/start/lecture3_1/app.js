d3e28ee44e78ff60cd13ce62fed9b9e9b85508b5
daniel vilchis <daniel.vilchis@accenture.com>
2023-04-24 14:33:05 -0600

    no message

--- WebXR/start/lecture3_1/app.js
+++ WebXR/start/lecture3_1/app.js
@@ -22,7 +22,7 @@ class App{
 		this.scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
 
         const light = new THREE.DirectionalLight( 0xffffff );
-        light.position.set( 1, 1, 1 ).normalize();
+        light.position.set( 1, 1, 1 ).normalize();
 		this.scene.add( light );
 			
 		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
